369b588416577b29d52d179452583205e83b8748
Andres Torres <andresalbertotorresgarcia@gmail.com>
2026-03-23 14:38:32 -0500

    feat: remove DeliverySection from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 import AboutSection from "@/components/sections/AboutSection";
 import CatalogSection from "@/components/sections/CatalogSection";
 import ContactSection from "@/components/sections/ContactSection";
-import DeliverySection from "@/components/sections/DeliverySection";
 import HeroSection from "@/components/sections/HeroSection";
 import TestimonialsSection from "@/components/sections/TestimonialsSection";
-import TrustBar from "@/components/sections/TrustBar";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <TrustBar />
       <CatalogSection />
       <TestimonialsSection />
-      <DeliverySection />
       <AboutSection />
       <ContactSection />
     </main>
